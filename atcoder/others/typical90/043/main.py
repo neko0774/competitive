33b8befc24a5f5941b7378f36997387b77f59f91
neko0774 <neko0774@gmil.com>
2023-06-13 08:31:12 +0900

    maze challange

--- atcoder/others/typical90/043/main.py
+++ atcoder/others/typical90/043/main.py
@@ -1,3 +1,5 @@
+import sys
+input = sys.stdin.readline
 H, W = map(int, input().split())
 sy, sx = map(int, input().split())
 gy, gx = map(int, input().split())
@@ -6,30 +8,34 @@ sx -= 1
 sy -= 1
 gx -= 1
 gy -= 1
-INF = 10**12
+INF = 10**9
 
 #0 -> right
 #1 -> left
 #2 -> up
 #3 -> down
+def change(x, y):
+    return y*W+x
 S = [[i for i in input()] for _ in range(H)]
 from collections import deque
 q = deque([[sx, sy, i] for i in range(4)])
-dist = [[[INF]*4 for _ in range(W)] for _ in range(H)]
+dist = [[INF]*4 for _ in range(W*H)]
 for i in range(4):
-    dist[sy][sx][i] = 0
+    dist[change(sx, sy)][i] = 0
 while q:
     print(q)
     x, y, d = q.popleft()
+    if x==gx and y==gy:
+        print(dist[change(gx, gy)][d])
+        exit()
     for i, j, nd in dxy:
         nx = x+i
         ny = y+j
-        if 0<=nx<W and 0<=ny<H and dist[ny][nx][nd]==INF and S[ny][nx]==".":
-            if d==nd:
-                dist[ny][nx][d] = dist[y][x][d]
+        if 0<=nx<W and 0<=ny<H and S[ny][nx]==".":
+            if d==nd and dist[change(nx, ny)][d]>dist[change(x, y)][d]:
+                dist[change(nx, ny)][d] = dist[change(x, y)][d]
                 q.appendleft([nx, ny, d])
-            else:
-                dist[ny][nx][nd] = dist[y][x][d]+1
+            elif dist[change(nx, ny)][nd]>dist[change(nx, ny)][d]+1:
+                dist[change(nx, ny)][nd] = dist[change(x, y)][d]+1
                 q.append([nx, ny, nd])
 print(dist)
-print(min(dist[gy][gx]))
