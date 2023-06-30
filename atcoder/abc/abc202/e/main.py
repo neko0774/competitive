8be2b1cd7958656f261d1413bd9c021cc8e20049
neko0774 <neko0774@gmail.com>
2023-07-01 00:43:47 +0900

    anything?

--- atcoder/abc/abc202/e/main.py
+++ atcoder/abc/abc202/e/main.py
@@ -2,7 +2,10 @@ N = int(input())
 P = [int(i) for i in input().split()]
 
 table = [[-1]*N.bit_length() for i in range(N)]
-
+from collections import deque
+q = deque([0])
+while q:
+  x 
 
 
 Q = int(input())
