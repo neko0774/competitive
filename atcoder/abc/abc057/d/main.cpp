323ba69efba8976e2be9906d1e780b620577c363
neko0774 <neko0774@gmail.com>
2023-12-12 22:27:18 +0900

    count not solve though

--- atcoder/abc/abc057/d/main.cpp
+++ atcoder/abc/abc057/d/main.cpp
@@ -1,24 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define ll long long
 
 
 int main(){
   int N, A, B;
   cin >> N >> A >> B;
-  vector<long long> V(N);
+  vector<ll> V(N);
   for(int i=0;i<N;i++){
     cin >> V[i];
   }
-  unordered_map<long long, int> cnt;
-  cnt[0]++;
-  for(int i=0;i<N;i++){
-    unordered_map<long long, int> nxt=cnt;
-    for(auto x: cnt){
-      nxt[x.first+V[i]]+=x.second;
-    }
-    cnt = nxt;
+  ll ans = 0;
+  sort(V.rbegin(), V.rend());
+  for(int i=0;i<A;i++){
+    ans += V[i];
   }
-  long long v = 0;
-  
+  ll M = N;
+  for(int i=0;i<N;i++){
+    if(V[i]<V[A-1]){
+      M = i;
+      break;
+    }
+  }
+  long double vv = (long double)ans/(long double)A;
+  vector<vector<unordered_map<ll, ll>>> dp(M+1, vector<unordered_map<ll, ll>>(B+1));
+  dp[0][0][0] = 1;
+  for(int i=0;i<M;i++){
+    for(int j=0;j<B+1;j++){
+      for(auto [value, count]:dp[i][j]){
+        if(j+1<=B&&V[i]>=V[A-1])dp[i+1][j+1][value+V[i]] += count;
+        dp[i+1][j][value] += count;
+      }
+    }
+  }
+  ans = 0;
+  for(int i=A;i<B+1;i++){
+    for(auto [value, count]:dp[M][i]){
+      if((long double)value/(long double)i==vv) ans += count;
+    }
+  }
+  cout << vv << endl;
+  cout << ans << endl;
 }
