c5514de29cdd71ff6e53569b5be7815f238496bb
neko0774 <neko0774@gmil.com>
2024-05-02 22:44:17 +0900

    abc nanika

--- atcoder/abc/abc226/e/main.cpp
+++ atcoder/abc/abc226/e/main.cpp
@@ -1,8 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+const ll mod = 988244353;
 
 
 
 int main(){
-
+  int N, M;
+  cin >> N >> M;
+  int u,v;
+  vector<vector<int>> G(N, vector<int>());
+  for(int i=0;i<M;i++){
+    cin >> u >> v;
+    u--;
+    v--;
+    G[u].push_back(v);
+    G[v].push_back(u);
+  }
+  int cnt = 0;
+  vector<bool> seen(N, false);
+  bool ok = true;
+  for(int i=0;i<N;i++){
+    if(seen[i]) continue;
+    queue<pair<int, int>> q;
+    seen[i] = true;
+    cnt++;
+    bool exist_cycle = false;
+    int edge = 0, vertex = 1;
+    q.push(make_pair(i, -1));
+    while(!q.empty()){
+      auto [x, lx] = q.front();q.pop();
+      edge += G[x].size();
+      for(auto nx:G[x]){
+        if(nx==lx){continue;}
+        else if(seen[nx]){
+          exist_cycle = true;
+        }else{
+          vertex++;
+          seen[nx] = true;
+          q.push(make_pair(nx, x));
+        }
+      }
+    }
+    if(!exist_cycle||edge/2!=vertex)ok=false;
+  }
+  ll ans = 1;
+  if(ok){
+    for(int i=0;i<cnt;i++){
+      ans *= 2;
+      ans %= mod;
+    }
+    cout << ans << endl;
+  }else{
+    cout << 0 << endl;
+  }
 }
--- atcoder/abc/abc350/f/main.py
+++ atcoder/abc/abc350/f/main.py
@@ -1,33 +1,4 @@
-S = ["("] + [i for i in input()] + [")"]
-import sys
-sys.setrecursionlimit(10**6)
-from collections import defaultdict
-blanket = defaultdict(int)
-cnt = 0
-for i, s in enumerate(S):
-  if s=="(":
-    last.append(i)
-    cnt += 1
-  elif s==")":
-    blanket[last.pop()] = i+1
-    cnt -= 1
-  elif cnt%2==0:
-    S[i] = S[i].swapcase()
+S = [i for i in input()]
 
-def solve(l, r):
-  ret = ""
-  l+=1
-  r-=1
-  while r-l>0:
-    if S[l]=="(":
-      if depth%2==0:
-        ret += solve(l, blanket[l])
-      else:
-        ret += solve(l, blanket[l])
-      l = blanket[l]
-    else:
-      ret += S[l]
-      l+=1
-  return ret
-
-print(solve(0, len(S)))
+def solve(ary, x):
+  
