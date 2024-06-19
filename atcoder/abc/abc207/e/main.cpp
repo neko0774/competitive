0655953e5d46e919c1ddc5761b8ee17ec5497d4a
neko0774 <neko0774@gmil.com>
2024-06-19 12:24:35 +0900

    a

--- atcoder/abc/abc207/e/main.cpp
+++ atcoder/abc/abc207/e/main.cpp
@@ -1,6 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#include<atcoder/modint>
+using mint = atcoder::modint1000000007;
 
 int main(){
-
+  int N;
+  cin >> N;
+  vector<ll> A(N);
+  for(int i=0;i<N;i++){
+    cin >> A[i];
+  }
+  vector<ll> B(N+1);
+  for(int i=0;i<N;i++){
+    B[i+1] += B[i] + A[i];
+  }
+  vector<vector<mint>> cnt(N+1, vector<mint>(N+1));
+  vector<mint> dp(N+1);
+  cnt[1][0] = 1;
+  for(int i=1;i<N+1;i++){
+    dp = vector<mint>(N+1);
+    for(int j=1;j<=N;j++){
+      dp[j] += cnt[j][B[i]%j];
+    }
+    for(int j=2;j<=N;j++){
+      cnt[j][B[i]%j] += dp[j-1];
+    }
+  }
+  mint ans;
+  for(int i=1;i<=N;i++){
+    ans += dp[i];
+  } 
+  cout << ans.val() << endl;
 }
--- atcoder/abc/abc211/e/main.cpp
+++ atcoder/abc/abc211/e/main.cpp
@@ -1,17 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-vector<vector<int>> grid;
+typedef unsigned long long ull;
+unordered_set<int> ans;
+unordered_set<ull> seen;
+vector<string> S(8);
+int memo = 0;
+ull cur = 0;
+int a = 0;
+vector<ll> dx={1,0,-1,0};
+vector<ll> dy={0,1,0,-1};
+
+bool ok(int x, int y, int N){
+  if(0<=x&&0<N&&0<=y&&y<N) return true;
+  return false;
+}
+
+
+void calc(int cx, int cy, int &K, int &N){
+  if(seen.count(cur)>0) return;
+  seen.insert(cur);
+  if(memo==K){
+    ans.insert(cur);
+    a++;
+    return;
+  }
+  vector<pair<int, int>> check;
+  for(int x=0;x<N;x++){
+    for(int y=0;y<N;y++){
+      if(S[y][x]!='.') continue;
+      bool connect = false;
+      for(int i=0;i<4;i++){
+        int nx = x+dx[i];
+        int ny = y+dy[i];
+        if(ok(nx, ny, N)&&S[ny][nx]=='r') connect = true;
+      }
+      if(connect) check.push_back(make_pair(x, y));
+    }
+  }
+  for(auto [nx, ny]: check){
+    S[ny][nx] = 'r';
+    memo++;
+    cur += ull(1)<<(ny*N+nx);
+    calc(nx, ny, K, N);
+    S[ny][nx] = '.';
+    memo--;
+    cur -= ull(1)<<(ny*N+nx);
+  }
+}
 
 int main(){
   int N, K;
   cin >> N >> K;
-  grid.resize(N, vector<int>(N));
+  S.resize(N);
   for(int i=0;i<N;i++){
-    for(int j=0;j<N;j++){
-      cin >> grid[i][j];
+    cin >> S[i];
+  }
+  for(int x=0;x<N;x++){
+    for(int y=0;y<N;y++){
+      if(S[y][x]=='.'){
+        memo = 1;
+        cur = ull(1)<<(y*N+x);
+
+        S[y][x] = 'r';
+        calc(x, y, K, N);
+        S[y][x] = '.';
+      }
     }
   }
-
-
+  cout << a << endl;
 }
