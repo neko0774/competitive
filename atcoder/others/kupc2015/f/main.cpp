7dc41233cb43b4b9f0c5717d55cb26fae772a95d
neko0774 <neko0774@gmil.com>
2024-06-20 14:52:00 +0900

    ?

--- atcoder/others/kupc2015/f/main.cpp
+++ atcoder/others/kupc2015/f/main.cpp
@@ -8,34 +8,69 @@ struct node{
   //s==1 -> op
   int s;
   int num;
-  string op;
-  node(int s, string op):s(s), op(op){};
-  node(int s, int num):s(s), num(num){};
+  char op;
+  int ord;
+  //pointerを使わないとき
+  int par = -1;
+  int left = -1;
+  int right = -1;
+
+  //pointerを使う
+  //node *par;
+  //node *left;
+  //node *right;
+
+  node(){}
+  //演算子の追加
+  node(int _s, char _op):s(_s), op(_op){}
+  //数字の追加 
+  node(int _s, int _num):s(_s), num(_num){}
+  //演算子の追加 子供の追加も同時に行う
+  node(int _s, char _op, int l, int r):s(_s), op(_op), left(l), right(r){}
+  //数字の追加 子供の追加も同時に行う
+  node(int _s, int _num, int l, int r):s(_s), num(_num), left(l), right(r){}
 };
 
-vector<node> conv(1<<20);
-vector<int> G[1<<20];
+vector<node> G(1<<20);
 
-void parse(string S, int num){
-  vector<int> st;
+int build(string S){
+  //受け取った逆ポーランド記法の文字列から木構造を作成する
+  //返り値は作成した木のroot
+  int cur = 0;
+  stack<int> st;
   for(auto s: S){
     if('0'<=s&&s<='9'){
-      st.push_back(--num);
-      conv[num] = node(0, s-'0');
+      G[cur] = node(0, s-'0');
     }else{
-      int ch1 = *st.rbegin();
-      st.pop_back();
-      int ch2 = *st.rbegin();
-      st.pop_back();
-      conv[--num] = node(1, s);
-      G[num].push_back(ch1);
-      G[num].push_back(ch2);
+      int right = st.top(); st.pop();
+      int left = st.top(); st.pop();
+      G[cur] = node(1, s, left, right);
+      G[left].par = cur;
+      G[right].par = cur;
     }
+    st.push(cur);
+    cur++;
   }
+  assert(st.size()==1);
+  return st.top();
+}
+
+void print(node cur){
+  if(cur.s==0) cout << cur.num;
+  if(cur.left!=-1)print(G[cur.left]);
+  if(cur.right!=-1)print(G[cur.right]);
+  if(cur.s==1) cout << cur.op;
 }
 
 int main(){
   string S;
   cin >> S;
-  parse(S, S.size());
+  int root = build(S);
+  int N = S.size();
+  for(int i=0;i<N;i++){
+    swap(G[i].left, G[i].right);
+    //cout << G[i].s << ' ' << G[i].left << ' ' << G[i].right << endl;
+  }
+  print(G[root]);
+  cout << endl;
 }
